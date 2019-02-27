91816f10606d097a18b574d643bf6c671fcd78cc
tchang9 <tlchang@gmail.com>
2019-02-27 16:28:13 -0500

    Bear Organizer

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Brown", "Nice", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 20, "Female", "Brown", "Angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 30, "Male", "Black", "Generous", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 40, "Male", "Brown", "Hangry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 50, "Female", "Polar", "Upset", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "Male", "Panda", "Altruistic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 80, "Male", "Brown", "Playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 80, "Male", "Brown", "Playful", 1);
