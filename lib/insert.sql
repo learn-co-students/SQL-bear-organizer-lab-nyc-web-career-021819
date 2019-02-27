74af3ac3e0d6304031405249cabce984e20c3074
Kate Kim <katekim5809@gmail.com>
2019-02-27 17:03:03 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr.Chocolate', 10, 'Male', 'Darkbrown' , 'flery', 0),
+('Rodwy', 6, 'Male', 'Brown' , 'playful', 0),
+('Tabitha', 16, 'Female', 'Brown' , 'gentle', 1),
+('Sergeant Brown', 11, 'Male', 'Black' , 'flery', 0),
+('Melissa', 4, 'Feale', 'Black' , 'playful', 0),
+('Grinch', 2, 'Male', 'White' , 'Calm', 0),
+('Wendy', 9, 'Female', 'Black&White' , 'lively', 0),
+(NULL, 9, 'Female', 'White' , 'violent', 0)
